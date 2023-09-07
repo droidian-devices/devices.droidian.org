bb9f95fab90bf84271457ddeb90464028626d66e
Virus288 <kiszczyckuba20@wp.pl>
2023-09-07 22:09:00 +0200

    Fix: Centerd devices in devices list, added auto-closing navbar after pressing any link
    Took 10 minutes

--- src/components/generic/views/Navbar.tsx
+++ src/components/generic/views/Navbar.tsx
@@ -3,13 +3,18 @@ import { HomeButton, NavbarBody, NavbarContainer, NavLinks, NavToggle } from '..
 import toggleNavbar from '../utils';
 import { Link } from '../../customs';
 
-const NavContent: React.FC<{ active: boolean }> = ({ active }) => {
+const NavContent: React.FC<{ active: boolean; setActive: React.Dispatch<React.SetStateAction<boolean>> }> = ({
+  active,
+  setActive,
+}) => {
   return (
     <NavLinks $active={active} $justify="space-evenly" $align="flex-start" $wrap="nowrap">
       <Link to="https://droidian.org/">Home</Link>
       <Link to="https://droidian.org/blog">Blog</Link>
       <Link to="https://github.com/orgs/droidian/projects?query=is%3Aopen">Projects</Link>
-      <Link to="/">Devices</Link>
+      <Link to="/" onClick={(): void => toggleNavbar(setActive, active)}>
+        Devices
+      </Link>
       <Link to="https://github.com/droidian-images/droidian/releases">Images</Link>
       <Link to="https://github.com/droidian/droidian/issues">Bug tracker</Link>
     </NavLinks>
@@ -31,7 +36,7 @@ const Navbar: React.FC = () => {
           <div />
         </NavToggle>
       </NavbarBody>
-      <NavContent active={active} />
+      <NavContent active={active} setActive={setActive} />
     </NavbarContainer>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
 import Home from './home/views/Component';
 import FourOhFour from './generic/views/FourOhFour';
 import DevicesCategories from './devicesCategories/views/Component';
-import DevicesList from './devicesList/views/Component';
 import Devices from './devices/views/Component';
 import Footer from './generic/views/Footer';
 import Test from './test/views/Component';
 
-export { Home, FourOhFour, Footer, Devices, DevicesCategories, DevicesList, Test };
+export { Home, FourOhFour, Footer, Devices, DevicesCategories, Test };
